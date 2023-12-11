77063d7eb0d4bf993324e18f587417247284b205
Alberto Macedo <alberto_junior-400@hotmail.com>
2023-12-11 13:09:37 -0300

    Listagem de usuários finalizada

--- administration/urls.py
+++ administration/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from administration.views.service_views import service_create, service_list, service_edit
-from administration.views.user_views import user_create
+from administration.views.user_views import user_create, user_list
 
 urlpatterns = [
     path('services/create/', service_create, name='service_create'),
     path('services/list/', service_list, name='service_list'),
     path('services/edit/<int:id>/', service_edit, name='service_edit'),
     path('users/create/', user_create, name='user_create'),
+    path('users/list/', user_list, name='user_list'),
 ]
--- administration/views/user_views.py
+++ administration/views/user_views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render, redirect
 
 from administration.forms.user_forms import UserForm
@@ -8,6 +9,13 @@ def user_create(request):
         user_form = UserForm(request.POST)
         if user_form.is_valid():
             user_form.save()
+            return redirect('user_list')
     else:
         user_form = UserForm()
     return render(request, 'users/user_form.html', {'user_form': user_form})
+
+
+def user_list(request):
+    User = get_user_model()
+    users = User.objects.filter(is_superuser=True)
+    return render(request, 'users/user_list.html', {'users': users})
